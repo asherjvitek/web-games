0c94af1c2d90ff4834aeaf83b1a6085144ab16a5
Asher Vitek <asher.vitek@gmail.com>
2025-02-07 07:32:32 -0600

    more drawing! More Cards! Getting closes to something that could be a real game. There are some bugs around moving the cards rightn now.

--- interaction/inter.js
+++ interaction/inter.js
@@ -3,34 +3,78 @@ const canvas = document.getElementById("canvas");
 /** @type {CanvasRenderingContext2D} */
 const context = canvas.getContext("2d");
 const leftClick = 1;
+const logInfo = false;
+const cardWidth = 100;
+const cardHeight = 170
+
+function log(...data) {
+    if (logInfo) {
+        console.info(data)
+    }
+}
+
+canvas.height = document.body.clientHeight;
+canvas.width = document.body.clientWidth;
 
 let state = {
-    card: {
-        x: 50,
-        y: 50,
-        w: 100,
-        h: 200
-    },
+    cards: [
+        {
+            x: 50,
+            y: 50,
+            w: cardWidth,
+            h: cardHeight,
+            color: "white"
+        }, {
+            x: 250,
+            y: 250,
+            w: cardWidth,
+            h: cardHeight,
+            color: "blue"
+        }
+    ],
     mouseClickPos: null
 }
 
-const canvasSpace = canvas.getBoundingClientRect()
+// const canvasSpace = canvas.getBoundingClientRect()
 
-context.fillStyle = "White";
-context.fillRect(50, 50, 100, 200);
+function clearCanvas() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+/** @function
+ * @argument {object[]} cards
+ */
+function drawCards(cards) {
+    for (let i = 0; i < cards.length; i++) {
+        const card = state.cards[i];
+
+        drawCard(card);
+    }
+}
+function drawCard(card) {
+    context.fillStyle = card.color;
+    context.fillRect(card.x, card.y, card.w, card.h);
+}
+
+drawCards(state.cards);
 
 canvas.addEventListener("mousedown", mousedown);
 canvas.addEventListener("mouseup", mouseup);
 canvas.addEventListener("mousemove", mousemove);
+
+window.onresize = (ev) => {
+    canvas.height = document.body.clientHeight;
+    canvas.width = document.body.clientWidth;
+    drawCards(state.cards);
+}
 
 /** @function
  * @name mousedown
 * @argument {MouseEvent} ev
 */
 function mousedown(ev) {
-    console.info(ev.clientX, ev.clientY, ev.button);
-    if (mounseInBox(ev.clientX, ev.clientY) && ev.buttons == leftClick) {
-        console.info("In the box");
+    log(ev.clientX, ev.clientY, ev.button);
+    if (mounseInCard(ev.clientX, ev.clientY) != null && ev.buttons == leftClick) {
         state.mouseClickPos = { x: ev.clientX, y: ev.clientY }
     }
 }
@@ -39,9 +83,7 @@ function mousedown(ev) {
 * @argument {MouseEvent} ev
 */
 function mouseup(ev) {
-    if (ev.buttons == leftClick) {
-        state.mouseClickPos = null;
-    }
+    state.mouseClickPos = null;
 }
 
 /** @function
@@ -60,31 +102,40 @@ function mousemove(ev) {
     currentMousePos = {
         x: ev.clientX,
         y: ev.clientY
-    }
+    };
 
     delta = {
         x: currentMousePos.x - state.mouseClickPos.x,
         y: currentMousePos.y - state.mouseClickPos.y
-    }
+    };
 
-    context.clearRect(state.card.x, state.card.y, state.card.w, state.card.h);
 
-    state.card.x += delta.x;
-    state.card.y += delta.y;
+    const card = mounseInCard(ev.clientX, ev.clientY);
 
-    context.fillRect(state.card.x, state.card.y, state.card.w, state.card.h);
+    card.x += delta.x;
+    card.y += delta.y;
+
+    clearCanvas();
+    drawCards(state.cards);
 
     state.mouseClickPos = {
         x: ev.clientX,
         y: ev.clientY
-    }
+    };
 }
 
 /** @function
  * argument {number} x
  * argument {number} x
 */
-function mounseInBox(x, y) {
-    return x >= state.card.x && y >= state.card.y && x <= state.card.x + state.card.w && y <= state.card.y + state.card.h;
+function mounseInCard(x, y) {
+    for (let i = 0; i < state.cards.length; i++) {
+        const card = state.cards[i];
+        if (x >= card.x && y >= card.y && x <= card.x + card.w && y <= card.y + card.h) {
+            return card;
+        }
+    }
+
+    return null;
 }
 
